4f401857d9ea98ae4ba3841d2faff70658953ef1
Santiago Cisneros <105238159+3Santiago1@users.noreply.github.com>
2022-05-29 22:04:31 -0700

    Add files via upload

---
+++ WEEK 3-1/code.js
@@ -0,0 +1,83 @@
+function blastoffTimerV1(){
+    console.log("blastoffTimerV1() started");
+    var countdownTimer = 50;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+        setTimeout(function(){
+            countdownTimer = countdownTimer - 5;
+             document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 5000)
+    setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 10000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 15000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 20000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 25000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 30000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 35000)
+      setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 40000)
+     setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer;
+    }, 45000)
+      setTimeout(function(){
+        countdownTimer = countdownTimer - 5;
+        document.getElementById("countdownDisplay").innerHTML = countdownTimer = "Blastoff!!!"
+    }, 50000)
+}
+//identifying variables as required by assignment
+var wins = 0;
+var losses = 0;
+var ties = 0;
+//start button as required for assignment
+function playCraps(){
+    console.log("playCraps() started");
+    var die1;
+    var die2;
+    //math function to play craps game as required by assignment
+    die1 = Math.ceil(Math.random() * 6);
+    die2 = Math.ceil(Math.random() * 6);
+    //results of rolled dice
+    console.log("Die1 = " + die1);
+    document.getElementById("die1Res").innerHTML = die1;
+    console.log("Die2 = " + die2); 
+    document.getElementById("die2Res").innerHTML = die2;
+    //create if sum from die1 and die2
+    var sum = die1 + die2;
+    //see if sum = 7 or 11
+    if(sum == 7 || sum == 11) {
+        //tallies of results after each spin
+        document.getElementById("crapsRes").innerHTML = "<h1>Craps, You lose!!!</h1>";
+        losses++;
+        document.getElementById("loseRes").innerHTML = losses;
+        console.log("Craps, You lose!!!");
+    } else if (die1 == die2 && die1 % 2 == 0) {
+        document.getElementById("crapsRes").innerHTML = "<h1>Huzzah, You Win!!!</h1>";
+        wins++;
+        document.getElementById("winRes").innerHTML = wins;
+        console.log("Huzzah, You win!!!")
+    } else {
+        document.getElementById("crapsRes").innerHTML = "<h2>You did not lose or win. Please try again.</h2>";
+        ties++;
+        document.getElementById("tieRes").innerHTML = ties;
+    }
+
+}
